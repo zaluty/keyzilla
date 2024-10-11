93c8d45d28e67c5744b9a1e8c0d61a99e3c678df
Hamza <hamzaredone6@gmail.com>
2024-10-11 22:19:58 +0000

    feat(apps/web) fix userid

--- apps/web/convex/cli.ts
+++ apps/web/convex/cli.ts
@@ -42,33 +42,41 @@ export const getApiKeys = query({
     }
 });
 export const getCliProjects = query({
-    args: { organizationId: v.optional(v.string()), enabled: v.optional(v.boolean()) },
+    args: { userId: v.string(), organizationId: v.optional(v.string()) },
     handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (identity === null) {
-            throw new Error("Not authenticated");
-        } 
-
-        const userId = identity.subject;
-
-        return ctx.db
+        if (!args.userId) {
+            return [];
+        }
+        const projects = await ctx.db
             .query("projects")
             .filter((q) => {
                 if (args.organizationId) {
                     return q.eq(q.field("organizationId"), args.organizationId);
                 } else {
-                    return q.or( 
-                        q.eq(q.field("organizationId"), undefined),
-                        q.eq(q.field("organizationId"), null)
+                    return q.and(
+                        q.eq(q.field("userId"), args.userId),
+                        q.or(
+                            q.eq(q.field("organizationId"), args.organizationId),
+                            q.eq(q.field("organizationId"), null)
+                        )
                     );
                 }
-            })
-            .collect()
-            .then(async (projects) => {
-                return projects.filter(project => project.allowedUsers?.includes(userId));
+            }).order("desc")
+            .collect().then((projects) => {
+                return projects.filter(project => project.allowedUsers?.includes(args.userId));
             });
+
+        const projectsWithApiKeys = await Promise.all(projects.map(async (project) => {
+            const apiKeys = await ctx.db
+                .query("apiKeys")
+                .filter((q) => q.eq(q.field("projectId"), project._id))
+                .collect();
+            return { ...project, apiKeys };
+        }));
+
+        return projectsWithApiKeys;
     },
-})
+});
 
 export const verify = query({
     args: { secretKey: v.string(), userId: v.string() },
