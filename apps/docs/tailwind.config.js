f97d3f2b44341bc20fb40d8807fc9a001cbc5ba2
Hamza <77211803+zaluty@users.noreply.github.com>
2024-10-22 11:14:30 +0000

    Update tailwind.config.js

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
     './app/**/*.{ts,tsx}',
     './content/**/*.{md,mdx}',
     './mdx-components.{ts,tsx}',
-    '../../node_modules/fumadocs-ui/dist/**/*.js',
+    './node_modules/fumadocs-ui/dist/**/*.js',
   ],
   presets: [createPreset()],
 };
